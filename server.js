bd49219264ddf0a5ea48c40e195959c2c195a69c
V S <vsptpm@gmail.com>
2020-09-07 14:19:07 +0530

    functional version

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const io = require('socket.io')(server);
 const {v4: uuidv4} = require('uuid');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -11,12 +12,22 @@ app.get('/',(req, res) => {
 })
 
 app.get('/:room',(req,res) => {
-    res.render('room', {roomId: req.params.room});
+    res.render('room', {roomId: req.params.room})
 
+})
+
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        
+        socket.join(roomId)
+        socket.to(roomId).broadcast.emit('user-connected', userId)
+        
+        socket.on('disconnect', () =>{
+            socket.to(roomId).broadcast.emit('user-disconnected',userId)
+        })
+    })
 })
 
 
 
-
-
-server.listen(3030);
+server.listen(3000);
